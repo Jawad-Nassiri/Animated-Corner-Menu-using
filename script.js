9be89ce1f5c8d77b1d888c955317edc4db2d2c6d
Jawad-Nassiri <jawadnasiri2222@gmail.com>
2025-04-23 10:08:56 +0200

    update javascript functionality'

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 const toggleBtn = document.querySelector('#toggle-btn')
-const accountIcon = document.querySelector('.account')
-const infoIcon = document.querySelector('.info')
-const messageIcon = document.querySelector('.message')
-const contactIcon = document.querySelector('.contact')
+const links = document.querySelectorAll('.menu a')
+
 
 
 
@@ -15,10 +13,10 @@ function openMenu() {
     isMenuOpen = !isMenuOpen
     toggleBtn.style.transform = isMenuOpen ?  "rotate(45deg)" : "rotate(0)";
 
-    accountIcon.style.transform = isMenuOpen ? "translate(150px, 0)" : "translate(0)"
-    infoIcon.style.transform = isMenuOpen ? "translate(150px, 90px)" : "translate(0)"
-    messageIcon.style.transform = isMenuOpen ? "translate(90px, 150px)" : "translate(0)"
-    contactIcon.style.transform = isMenuOpen ? "translate(0, 150px)" : "translate(0)"
+    links[0].style.transform = isMenuOpen ? "translate(150px, 0)" : "translate(0)"
+    links[1].style.transform = isMenuOpen ? "translate(150px, 90px)" : "translate(0)"
+    links[2].style.transform = isMenuOpen ? "translate(90px, 150px)" : "translate(0)"
+    links[3].style.transform = isMenuOpen ? "translate(0, 150px)" : "translate(0)"
 }
 
 toggleBtn.addEventListener('click', openMenu)
